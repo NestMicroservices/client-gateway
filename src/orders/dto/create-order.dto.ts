e55d42990d710640167a69f80c885b9fbc569ec8
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-04-12 13:20:59 -0500

    order-item.dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,28 +1,12 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatus, orderStatusList } from '../interfaces';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
-  @IsEnum(orderStatusList, {
-    message: `status must be one of the following values: ${orderStatusList}`,
-  })
-  @IsOptional()
-  status: OrderStatus = 'PENDING';
-
-  @IsBoolean()
-  @IsOptional()
-  paid?: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
